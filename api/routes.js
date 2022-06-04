0d97dab2be37b6496218a747a07435d3f63cf44f
Andre Marquez <andre.marqquez@gmail.com>
2022-06-04 12:16:43 -0300

    feat: arquivos banco de dados configurados

--- api/routes.js
+++ api/routes.js
@@ -2,6 +2,20 @@ import Router from '@koa/router';
 
 export const router = new Router();
 
- router.get('/tweets', (ctx) => {
-  ctx.body = 'tweets';
+const tweets = [];
+
+router.get('/tweets', (ctx) => {
+  ctx.body = ctx.query.username
+    ? tweets.filter((tweet) => tweet.username === ctx.query.username)
+    : tweets;
+});
+
+router.post('/tweets', (ctx) => {
+  const tweet = {
+    ...ctx.request.body,
+    id: tweets.length + 1,
+  };
+
+  tweets.push(tweet);
+  ctx.body = tweet;
 });
--- api/setup.js
+++ api/setup.js
@@ -1,8 +1,12 @@
 import Koa from 'koa';
-import { router } from './routes';
+import { router } from './routes.js';
+import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 
 export const app = new Koa();
 
+app.use(cors());
+app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
 app.use((ctx) => {
