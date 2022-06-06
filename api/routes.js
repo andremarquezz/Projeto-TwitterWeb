8e9d9476e36fe5534c08f32a1c0cdbe7feff8c9f
Andre Marquez <andre.marqquez@gmail.com>
2022-06-06 19:24:54 -0300

    feat: autenticação de usuario

--- api/routes.js
+++ api/routes.js
@@ -25,18 +25,60 @@ router.post('/tweets', async (ctx) => {
 router.post('/signup', async (ctx) => {
   const saltRounds = 10;
   const password = bcrypt.hashSync(ctx.request.body.password, saltRounds);
-  const user = await prisma.user.create({
-    data: {
-      name: ctx.request.body.name,
-      username: ctx.request.body.username,
-      email: ctx.request.body.email,
-      password,
-    },
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        name: ctx.request.body.name,
+        username: ctx.request.body.username,
+        email: ctx.request.body.email,
+        password,
+      },
+    });
+
+    ctx.body = {
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+    };
+  } catch (error) {
+    if (error.meta && !error.meta.target) {
+      ctx.status = 422;
+      ctx.body = 'Email ou nome de usuário já existe.';
+      return;
+    }
+
+    ctx.status = 500;
+    ctx.body = 'Internal error';
+  }
+});
+
+router.get('/login', async (ctx) => {
+  const [, token] = ctx.request.headers.authorization.split(' ');
+  const [email, plainTextPassword] = Buffer.from(token, 'base64').toString().split(':');
+
+  const user = await prisma.user.findUnique({
+    where: { email },
   });
-  ctx.body = {
-    id: user.id,
-    name: user.name,
-    username: user.username,
-    email: user.email,
-  };
+
+  console.log({ user });
+
+  if (!user) {
+    ctx.status = 404;
+    return;
+  }
+
+  const passwordMatch = bcrypt.compareSync(plainTextPassword, user.password);
+
+  if (passwordMatch) {
+    ctx.body = {
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+    };
+    return;
+  }
+  ctx.status = 404;
 });
