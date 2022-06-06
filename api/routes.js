c4a18a1a28309612913a0d37696bd738e8ce259f
Andre Marquez <andre.marqquez@gmail.com>
2022-06-06 18:31:10 -0300

    fix: retorna para aplicação funcionando sem criptgrafia no password

--- api/routes.js
+++ api/routes.js
@@ -11,14 +11,24 @@ router.get('/tweets', async (ctx) => {
 });
 
 router.post('/tweets', async (ctx) => {
-  const tweet = {
-    userId: 'cl404x10m0009ly9bbp6v12f1',
-    text: ctx.request.body.text,
-  };
-
-  const doc = await prisma.tweet.create({
-    data: tweet,
+  const tweet = await prisma.tweet.create({
+    data: {
+      userId: 'cl404x10m0009ly9bbp6v12f1',
+      text: ctx.request.body.text,
+    },
   });
 
-  ctx.body = doc;
+  ctx.body = tweet;
+});
+
+router.post('/signup', async (ctx) => {
+  const user = await prisma.user.create({
+    data: {
+      name: ctx.request.body.name,
+      username: ctx.request.body.username,
+      email: ctx.request.body.email,
+      password: ctx.request.body.password,
+    },
+  });
+  ctx.body = user;
 });
